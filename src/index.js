f7f49d77d3d55ea0d9ab83ce076f34a07c848a0e
shree-2802 <pattupadmashree@gmail.com>
2024-01-19 17:47:39 +0530

    js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
 
-const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+import Routing from './Route/Router';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <Routing />
+  </Provider>
+);
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import {productReducer} from './productReducer';
+
+export const reducers=combineReducers({
+    allProducts:productReducer
+})
